526be2f598b97ba66f8e4f4f1ab806b0f4636c57
austintip <austin.d.tipograph@gmail.com>
2021-02-24 17:54:42 -0500

    about to pull

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ function App() {
         currentUser={currentUser} 
         handleAuth={handleAuth}
         setContent={setContent}
-        content={content}
       />
       <Content 
         currentUser={currentUser}
