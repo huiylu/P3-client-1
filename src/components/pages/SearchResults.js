3e398ed3ab667fd995fe27e249886ab30257c638
edJaPe <edv06jara@gmail.com>
2021-02-24 09:32:37 -0800

    got songs to show in showResult

--- src/components/pages/SearchResults.js
+++ src/components/pages/SearchResults.js
@@ -1,10 +1,15 @@
 const SearchResults = (props) => {
-    {console.log('💰', props.content.data)}
+    { console.log('💰', props.content.data) }
+    let songList = props.content.data.song.map((song, i) => (
+        <li>
+            <h1 key={i} >{song.name}</h1>
+        </li>
+    ))
     return (
         <div className="container">
+            <h1>THIS IS WHERE YOUR SEARCH RESULTS WILL BE</h1>
             <div className="search-results">
-                <h1>THIS IS WHERE YOUR SEARCH RESULTS WILL BE</h1>
-                <h1>{props.searchQuery}</h1>
+                {songList}
             </div>
         </div>
     );
