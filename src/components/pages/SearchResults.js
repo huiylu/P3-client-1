1c9b92b775783cfd9f64f95d86acf91eeda82c6e
huiylu <74116599+huiylu@users.noreply.github.com>
2021-02-25 15:05:08 -0800

    updated

--- src/components/pages/SearchResults.js
+++ src/components/pages/SearchResults.js
@@ -2,10 +2,6 @@ import SongSearch from '../partials/SongSearch'
 
 
 const SearchResults = (props) => {
-
-<<<<<<< HEAD
-    { console.log('🙏🏼', props.content) }
-=======
     { console.log('🙏🏼', props.content.length) }
     let songList
 
@@ -20,13 +16,6 @@ const SearchResults = (props) => {
     } else {
         <h1>Loading</h1>
     }
->>>>>>> 5736fb312102abf0364b42e5bddf441fe11f9b4c
-
-    // let songList = props.content.data.song.map((song, i) => (
-    //     <li className="song-list-result">
-    //         <h1 key={i}>{song.name}</h1>
-    //     </li>
-    // ))
     return (
         <div className="container">
             {/* <h1>THIS IS WHERE YOUR SEARCH RESULTS WILL BE</h1> */}
@@ -34,11 +23,7 @@ const SearchResults = (props) => {
                 <h1>THIS IS WHERE YOUR SEARCH RESULTS WILL BE</h1>
                 {/* <li className="song-card">This is the song card.</li> */}
                 {/* <PlaylistCard songList={songList} /> */}
-<<<<<<< HEAD
-                <SongSearch />
-=======
                 <SongSearch songList={songList} />
->>>>>>> 5736fb312102abf0364b42e5bddf441fe11f9b4c
             </div>
         </div>
     );
