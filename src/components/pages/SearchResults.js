2ad504c91b93cc97da89268d839246592b71abf0
austintip <austin.d.tipograph@gmail.com>
2021-02-25 13:36:59 -0500

    added styling

--- src/components/pages/SearchResults.js
+++ src/components/pages/SearchResults.js
@@ -1,24 +1,23 @@
-import PlaylistCard from '../partials/PlaylistCard'
+import SongSearch from '../partials/SongSearch'
 
 
 const SearchResults = (props) => {
 
     { console.log('🙏🏼', props.content) }
 
-    let songList = props.content.data.song.map((song, i) => (
-        <li className="song-list-result">
-            <h1 key={i}>{song.name}</h1>
-        </li>
-    ))
+    // let songList = props.content.data.song.map((song, i) => (
+    //     <li className="song-list-result">
+    //         <h1 key={i}>{song.name}</h1>
+    //     </li>
+    // ))
     return (
         <div className="container">
             {/* <h1>THIS IS WHERE YOUR SEARCH RESULTS WILL BE</h1> */}
-            <div className="search-results">
+            <div className="inner-container">
                 <h1>THIS IS WHERE YOUR SEARCH RESULTS WILL BE</h1>
-
-                <h1>song</h1>
-                <PlaylistCard songList={songList} />
-
+                {/* <li className="song-card">This is the song card.</li> */}
+                {/* <PlaylistCard songList={songList} /> */}
+                <SongSearch />
             </div>
         </div>
     );
