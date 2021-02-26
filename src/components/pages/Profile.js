486e506083fafaaf3c37686c34b7b9283230ed99
huiylu <74116599+huiylu@users.noreply.github.com>
2021-02-25 16:10:54 -0800

    Commented out form

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -75,13 +75,13 @@ const Profile = (props) => {
     <div className="container">
       <h1>Welcome to your Playlists</h1>
       <h4>Select one of your playlists to look through your songs.</h4>
-      <form onSubmit={createPlaylist()}>
+     
         <div className="form-elem">
           <label htmlFor="title">Playlist Title</label>
           <input type="text" name="title" placeholder="Title of your Playlist" onChange={e => props.setTitle(e.target.value)} />
           <input type="submit" value="Create Playlist" />
         </div>
-      </form>
+      
       <div className="inner-container">
         <PlaylistCard
           deletePlaylist={deletePlaylist} />
